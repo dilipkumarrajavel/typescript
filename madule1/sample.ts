2d36bd4c9c6cceed9e3353fa43f99c46a637f05f
dilipkumarrajavel <dilipkumarrajavel29@gmail.com>
2026-06-25 21:17:52 +0530

    functions

---
+++ madule1/sample.ts
@@ -0,0 +1,9 @@
+//practiec 1
+let user: string="Hello,Dilip welcome to playwright";
+console.log((user));
+////practiec 2
+let name: string="Dilip Kumar";
+const age: number=27;
+const dob: number=29/5/1999;
+const nationality: string="indian";
+console.log
