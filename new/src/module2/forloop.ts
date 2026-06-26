0cacd2503e85dbaec31a3e6d50351aa15bdd5108
dilipkumarrajavel <dilipkumarrajavel29@gmail.com>
2026-06-26 11:56:27 +0530

    done

--- new/src/module2/forloop.ts
+++ new/src/module2/forloop.ts
@@ -1,14 +1,2 @@
-for (let i = 1; i <=10; i++ ) {
-    console . log("count:", i) ;
-}
 
-for (let d = 1; d <=100; d++) {
-    console . log("match:", d) ;
-    
-}
-let n:number = 5;
-for(let i:number = 1; i <= n; i++);{
-    let row:string = "";
-    for (j: number = 1; i <= n-1; j++)
-}
  
---
+++ new/src/module2/var,const,let.ts
@@ -0,0 +1,5 @@
+var city = "Chennai"; // Old
+let name = "Dilip Kumar"; // Can change
+const Pincode = 600011; // Constant
+name = "R. Dilip Kumar"; 
+console.log("name".toLocaleUpperCase,city,name,Pincode);
