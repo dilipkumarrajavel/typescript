02cd917d30a80ea46c6b2dbd7c08e9fa9ae233c3
dilipkumarrajavel <dilipkumarrajavel29@gmail.com>
2026-06-27 13:33:36 +0530

    ok

--- new/src/rough/example.ts
+++ new/src/rough/example.ts
@@ -8,9 +8,14 @@ if (isRaining) {
  console.log("Take an umbrella");
 } else {
  console.log("Enjoy your day");}
-
+//example:
  let issunday: boolean = false;
  if(issunday) {console.log("play cricket");
  } else{console.log("Noooo");}
  
- 
+ //if, else if, else statement:
+ let marks: number=30;
+ if(marks>=90) {console.log("rank1");}
+ else if(marks>=75) {console.log("rank2");}
+ else if(marks>=47) {console.log("rank3");}
+ else{console.log("fail");}
