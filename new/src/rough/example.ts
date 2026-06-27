683adaae5e65b88319261b6b5463d7ae6ada4f21
dilipkumarrajavel <dilipkumarrajavel29@gmail.com>
2026-06-27 12:36:23 +0530

    ok

---
+++ new/src/rough/example.ts
@@ -0,0 +1,16 @@
+//if statement:
+let age:number=20;
+if(age>=18){console.log("you are eligible to vote.")}
+
+//if else statement:
+let isRaining: boolean = false;
+if (isRaining) {
+ console.log("Take an umbrella");
+} else {
+ console.log("Enjoy your day");}
+
+ let issunday: boolean = false;
+ if(issunday) {console.log("play cricket");
+ } else{console.log("Noooo");}
+ 
+ 
