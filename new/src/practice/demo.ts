ce572e546b387ad26eba67269918ed5528d6740b
dilipkumarrajavel <dilipkumarrajavel29@gmail.com>
2026-07-01 15:18:08 +0530

    ok

--- new/src/practice/demo.ts
+++ new/src/practice/demo.ts
@@ -1,60 +1,72 @@
-// // // // // // let giriMap = new Map<string, number>();
-// // // // // // giriMap.set("marks", 90);
-// // // // // // console.log(giriMap);
+// // // // // // // let giriMap = new Map<string, number>();
+// // // // // // // giriMap.set("marks", 90);
+// // // // // // // console.log(giriMap);
 
-// // // // // let info: {name:string; 
-// // // // //            age:number;} = {
-// // // // //     name:"dilip",
-// // // // //     age:27
-// // // // // }
-// // // // // console.log(info)
+// // // // // // let info: {name:string; 
+// // // // // //            age:number;} = {
+// // // // // //     name:"dilip",
+// // // // // //     age:27
+// // // // // // }
+// // // // // // console.log(info)
 
-// // // // let emp: { id?: number; email: string } = { email: "dilip" };
-// // // // console.log(emp);
-// // // enum day {
-// // //   monday,
-// // //   tuesday,
-// // //   wednesday
+// // // // // let emp: { id?: number; email: string } = { email: "dilip" };
+// // // // // console.log(emp);
+// // // // enum day {
+// // // //   monday,
+// // // //   tuesday,
+// // // //   wednesday
+// // // // }
+// // // // let today: day = day.wednesday;
+// // // // console.log(today);
+
+// // // // function add(a:number, b:number): number{ 
+// // // //     return a+b;
+// // // // }
+// // // // console.log(1000+3000)
+// // // function add(a:number, b:number, c:number): number{
+// // //     return a+b*c;
 // // // }
-// // // let today: day = day.wednesday;
-// // // console.log(today);
+// // // console.log(10+34*2)
 
-// // // function add(a:number, b:number): number{ 
-// // //     return a+b;
-// // // }
-// // // console.log(1000+3000)
-// // function add(a:number, b:number, c:number): number{
-// //     return a+b*c;
+// // function name(): void {
+// //   console.log("Hello Giri!");
 // // }
-// // console.log(10+34*2)
+// // name();
 
-// function name(): void {
-//   console.log("Hello Giri!");
+// // function greet(): void {
+// //     console.log("Hello Dilip")
+// // }greet();
+
+// function yeah(): void{
+//     console.log("Hai Dilip Kumar")
+// }yeah();
+
+// let message = `Welcome, ${name}!`;
+// console.log(message);
+
+// let Dilip: readonly number[]=[1,2,3,4,5];
+// console.log(Dilip)
+// const add: number[]=[1,2,3,4,5];
+// console.log(add)
+
+// type GiriType = { id: number; username: string };
+// let giriUser: GiriType = { id: 1, username: "giri" };
+// console.log(giriUser);
+
+// type dilip = {id:number; Username:string; age:number};
+// var istester: boolean = true;
+// let usersilip: dilip = {id: 18, Username:"kumar", age:27};
+// console.log(istester)
+// console.log(usersilip)
+
+//normal function:
+// function add(a:number, b:number): number{
+//     return a + b;
 // }
-// name();
+// console.log(999 + 2000);
 
-// function greet(): void {
-//     console.log("Hello Dilip")
-// }greet();
-
-function yeah(): void{
-    console.log("Hai Dilip Kumar")
-}yeah();
-
-let message = `Welcome, ${name}!`;
-console.log(message);
-
-let Dilip: readonly number[]=[1,2,3,4,5];
-console.log(Dilip)
-const add: number[]=[1,2,3,4,5];
-console.log(add)
-
-type GiriType = { id: number; username: string };
-let giriUser: GiriType = { id: 1, username: "giri" };
-console.log(giriUser);
-
-type dilip = {id:number; Username:string; age:number};
-var istester: boolean = true;
-let usersilip: dilip = {id: 18, Username:"kumar", age:27};
-console.log(istester)
-console.log(usersilip)
+//Arrow Function:
+const greet=(name:string):string =>{
+    return "Hello,"+name;
+}
+console.log(greet("Dilip Kumar"));
