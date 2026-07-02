27737b4d6c99095bb6de5a19c5322c50a086c96f
dilipkumarrajavel <dilipkumarrajavel29@gmail.com>
2026-07-02 14:27:32 +0530

    folder structure has changed

--- new/src/typescript lessons/module 1&2/dataType.ts
+++ new/src/typescript lessons/module 1&2/dataType.ts
@@ -1,19 +1,19 @@
 
-// let fullName: string = "Dilipkumar";
-// let age: number = 27;
-// let isTester: boolean = true;
-// let score: any = 100;
-// let value: unknown = "Playwright";
-// console.log(("fullName:"),("Dilipkumar").toLocaleUpperCase());
-// console.log(("age:"),age);
-// console.log(("isTester:"),isTester);
-// console.log(("score:"),score);
-// console.log(("value:"),value);
+let fullName: string = "Dilipkumar";
+let limit: number = 27;
+let isTester: boolean = true;
+let score: any = 100;
+let stage: unknown = "Playwright";
+console.log(("fullName:"),("Dilipkumar").toLocaleUpperCase());
+console.log(("age:"),limit);
+console.log(("isTester:"),isTester);
+console.log(("score:"),score);
+console.log(("value:"),stage);
 
 
-// //below the console also can use
+//below the console also can use
 
-// console.log(fullName, age, isTester, score, value);
+console.log(fullName, limit, isTester, score, stage);
 
 let age: number=27;
 console.log("Age:",age)
